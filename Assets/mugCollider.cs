49dfe19ca07f0b99bb6b9480c086903bcad42934
Jasmine-Lapierre <89647613+Jasmine-Lapierre@users.noreply.github.com>
2023-12-04 23:40:10 -0500

    Ajout de la poubelle

--- Assets/mugCollider.cs
+++ Assets/mugCollider.cs
@@ -7,11 +7,18 @@ public class mugCollider : MonoBehaviour
 
   public void OnTriggerEnter(Collider other)
     {
-                    Debug.Log("collision");
+                   
 
         if(other.gameObject.layer == 7){
             Debug.Log("collision7");
             levelManager.Instance.addElement(other.gameObject.name);
+        }
+        if(other.gameObject.tag =="trash"){
+            GameObject MugTag = GameObject.FindGameObjectWithTag("mug");
+            Transform[] children = MugTag.GetComponentsInChildren<Transform>();
+            for(int i = 0; i < children.Length; i++){
+                MugTag.transform.GetChild(i).gameObject.SetActive(false);
+            }
         }
     }
 
